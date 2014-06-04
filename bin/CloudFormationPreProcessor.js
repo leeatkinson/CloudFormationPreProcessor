3e2e03f3cf02851e414a0dd24cfaaa82beb2104a
Lee Atkinson <lee@rocketindigo.com>
2014-06-04 12:50:42 +0100

    Fixed error with undefined processed template extension

--- bin/CloudFormationPreProcessor.js
+++ bin/CloudFormationPreProcessor.js
@@ -15,12 +15,12 @@ var lib = require(libPath);
 function collect(val, m) { m.push(val); return m; }
 
 commander
-.version("0.0.1")
+.version("0.0.2")
 .option("-r --region [region-name]", "AWS region [eu-west-1]", "eu-west-1")
 .option("-d --working-directory [working-directory]", "Working directory [current working directory]")
 .option("-f --config-file-pattern [config-file-pattern]", "Config file pattern [*.cfnpp]", collect, [ ])
 .option("-c --compact", "Compact template")
-.option("-e --extension [extension]", "Processed extension - if blank, overwrites template []", "")
+.option("-e --processed-template-extension [extension]", "Processed template extension - if blank, overwrites template []", "")
 .parse(process.argv);
 
-lib(commander.region, commander.configFilePattern, commander.workingDirectory, commander.compact, commander.processedExtension);
+lib(commander.region, commander.configFilePattern, commander.workingDirectory, commander.compact, commander.processedTemplateExtension);
