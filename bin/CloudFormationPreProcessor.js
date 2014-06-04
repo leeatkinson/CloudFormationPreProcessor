ec719d3d66947dd0805d6685707be9a2adb4903a
Lee Atkinson <lee@rocketindigo.com>
2014-06-04 14:42:04 +0100

    Added commander.description and output version on execution

--- bin/CloudFormationPreProcessor.js
+++ bin/CloudFormationPreProcessor.js
@@ -2,8 +2,6 @@
 
 "use strict";
 
-console.log("CloudFormation Pre-Processor");
-
 var path = require("path");
 var fs = require("fs");
 
@@ -12,15 +10,16 @@ var commander = require("commander");
 var libPath = path.join(path.dirname(fs.realpathSync(__filename)), "../lib/CloudFormationPreProcessor.js");
 var lib = require(libPath);
 
-function collect(val, m) { m.push(val); return m; }
+function collect(o, a) { a.push(o); return a; }
 
-commander
-.version("1.0.0")
+commander.description("CloudFormation Pre-Processor").version("1.1.0")
 .option("-r --region [region-name]", "AWS region. [eu-west-1]", "eu-west-1")
 .option("-d --working-directory [working-directory]", "Working directory. [current working directory]")
 .option("-f --config-file-pattern [config-file-pattern]", "Config file pattern. [*.cfnpp]", collect, [ ])
 .option("-c --compact", "Compact template. [true]")
 .option("-e --processed-template-extension [extension]", "Processed template extension. If blank, overwrites template. []", "")
 .parse(process.argv);
+
+console.log(commander.description() + " " + commander.version());
 
 lib(commander.region, commander.configFilePattern, commander.workingDirectory, commander.compact, commander.processedTemplateExtension);
