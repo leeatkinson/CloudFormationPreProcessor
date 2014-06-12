917ccac5f3e5f13c1185edef4212be4caa6d08bc
Lee Atkinson <lee@rocketindigo.com>
2014-06-12 16:19:46 +0100

    Renamed to main.js files

--- bin/main.js
+++ bin/main.js
@@ -7,12 +7,12 @@ var fs = require("fs");
 
 var commander = require("commander");
 
-var libPath = path.join(path.dirname(fs.realpathSync(__filename)), "../lib/CloudFormationPreProcessor.js");
+var libPath = path.join(path.dirname(fs.realpathSync(__filename)), "../lib/main.js");
 var lib = require(libPath);
 
 function collect(o, a) { a.push(o); return a; }
 
-commander.description("CloudFormation Pre-Processor").version("1.2.1")
+commander.description("CloudFormation Pre-Processor").version("1.3.0")
 .option("-r --region [region-name]", "AWS region. [eu-west-1]", "eu-west-1")
 .option("-d --working-directory [working-directory]", "Working directory. [current working directory]")
 .option("-f --config-file-pattern [config-file-pattern]", "Config file pattern. [*.cfnpp]", collect, [ ])
