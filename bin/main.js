8fa3536319b69e1e2be57fe2b4f93f43156f9d78
Lee Atkinson <lee@rocketindigo.com>
2014-08-19 11:47:15 +0100

    Includes defined by folder structure
    No longer required to have a config.

--- bin/main.js
+++ bin/main.js
@@ -16,19 +16,13 @@ var packageJson = require("../package.json");
 
 commander.description("CloudFormation Pre-Processor").version(packageJson.version)
 .option("-r --region [region-name]", "AWS region. [eu-west-1]", "eu-west-1")
-.option("-d --working-directory [working-directory]", "Working directory. [current working directory]")
-.option("-f --config-file-pattern [config-file-pattern]", "Config file pattern. [*.cfnpp]", collect, [ ])
-.option("-c --compact", "Compact template. [true]")
-.option("-e --processed-template-extension [extension]", "Processed template extension. If blank, overwrites template. []", "")
+.option("-f --template-file-pattern [template-file-pattern]", "Template file pattern. [./*.cloudformation]", collect, [ ])
 .parse(process.argv);
 
 console.log(commander.description() + " " + commander.version());
 
 lib(commander.region, 
-	commander.configFilePattern, 
-	commander.workingDirectory, 
-	commander.compact, 
-	commander.processedTemplateExtension,
-	function(err) {
+	commander.templateFilePattern, 
+	function(error) {
 		console.log("Done!");
 	});
