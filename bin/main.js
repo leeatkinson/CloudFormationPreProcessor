0ed230eb042b14a2fa9f484ed6598bcdf93268fe
Lee Atkinson <lee@rocketindigo.com>
2014-10-03 12:52:21 +0100

    Removed template named arguments

--- bin/main.js
+++ bin/main.js
@@ -13,15 +13,12 @@ var lib = require(libPath);
 var packageJson = require("../package.json");
 
 commander.description("CloudFormation Pre-Processor").version(packageJson.version)
-	.option("-r --region [region]", "AWS region. [eu-west-1]", "eu-west-1")
-	.option("-t --template [template]", "Template path pattern. [./*.cloudformation]", function(o, a) { 
-		a.push(o); 
-		return a; 
-	}, [ ])
+	.usage("[options] <template-path-pattern> ...")
+	.option("-r, --region [region]", "AWS region [eu-west-1]", "eu-west-1")
 	.parse(process.argv);
 
 console.log(commander.description() + " " + commander.version());
 
-lib(commander.region, commander.template, function(error) {
+lib(commander.region, commander.args, function(error) {
 	console.log("Done!");
 });
